4028736571c84db2ca49257b55d12f020ae02f97
Fabiola <fabiolaeh9414@gmail.com>
2019-11-17 12:53:28 -0600

    Configuring spark context

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,28 @@
 package app;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("PageRank");
+
+        // Setup Spark config
+        SparkConf conf = new SparkConf()
+                .setAppName("PageRankApplication")
+                .setMaster("local[*]"); // Run Spark locally with as many worker threads as logical cores in the machine
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // Processing
+        System.out.println("Calculating PageRank");
+
+        // Stop Spark
+        sc.close();
+        sc.stop();
     }
-
-
+    
 }
 
 
