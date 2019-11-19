03ad53bf59b56c963b10c1a1919cea968409d3a6
Barnum Castillo Barquero <barnum2011@live.com>
2019-11-18 21:21:29 -0600

    Agrega cuestiones de parámetros al main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,10 +5,18 @@ import app.spark.SparkManager;
 import app.utils.Utils;
 import org.apache.spark.api.java.JavaPairRDD;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        final String FILE_PATH = "/home/fabiola/IdeaProjects/pagerank/src/main/java/app/data/input_data.txt"; // ToDo: cambiar para que acepte path relativo
+
+        final String FILE_PATH = "C:/Users/barnu/Desktop/pagerank/src/main/java/app/data/input_data.txt"; // ToDo: cambiar para que acepte path relativo
+
+
         SparkManager sparkManager = SparkManager.getInstance();
         Utils utils = new Utils();
 
@@ -18,9 +26,20 @@ public class Main {
         // Generate graph structure
         JavaPairRDD<String, Iterable<String>> inputGraph = utils.buildGraph(FILE_PATH);
 
+        // Reads the dampening factor
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please enter the dampening factor (default=0.85): ");
+        String dFactor = myObj.nextLine();  // Read user input
+        double dampeningFactor = 0.85;
+        try {
+            dampeningFactor = new Double(dFactor);
+        } catch (Exception e) {
+            // Keep the default value
+        }
+
         // Processing
         System.out.println("Calculating PageRank");
-        PageRank.calculatePageRank(inputGraph, 0.85);
+        PageRank.calculatePageRank(inputGraph, dampeningFactor);
 
         // Stop Spark
         sparkManager.stopSpark();
